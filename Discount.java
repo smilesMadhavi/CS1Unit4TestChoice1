924bdff7a709c180590114dd54ab5f2d0511c02a
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-11-05 12:21:54 -0600

    Add files via upload

---
+++ Discount.java
@@ -0,0 +1,22 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - 
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class Discount
+{
+	
+	
+	//getDiscountedBill() will return final amount of the bill
+	//			if the bill is >2000, the bill receives a 15% discount
+	public static int getDiscountedBill(int bill)
+	{
+		return 0;
+	}
+}
---
+++ DiscountRunner.java
@@ -0,0 +1,23 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - 
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class DiscountRunner
+{
+	public static void main( String args[] )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Enter the original bill amount :: ");
+		int amt = keyboard.nextInt();
+		
+
+	}
+}
